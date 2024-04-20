e99814522fbcde11b9657a21003d2a1781504d1e
Nicholas Cochrane <nicholascochrane25@gmail.com>
2024-04-19 18:41:47 -0700

    Added Default message sound
    Messages that are empty after formatting can play a default sound under /sounds/default.wav
    
    Messages that consist of only one emote (ignoring emoji)  can play a sounds based on that emote or the defualt message sound

--- vox.py
+++ vox.py
@@ -6,6 +6,7 @@ import os
 import re
 import json
 import socket
+import struct
 
 CHUNK = 1024
 SERVER = 'irc.chat.twitch.tv'
@@ -14,19 +15,19 @@ NICKNAME = 'ChatReader' #Bot's nickname here
 TOKEN = 'PLACE TOKEN HERE' #format 'oauth:YOURTOKENHERE'
 CHANNEL = 'PLACE CHANNEL NAME HERE WITH # IN FRONT'  #format '#Channelname'
 
-USE_BOUYOMICHAN = True
-if USE_BOUYOMICHAN:
-    import bouyomichan
-else:
-    from voicevox import Client
-    import pyaudio
 
+USE_BOUYOMICHAN = True #use Bouyomichan else use VoiceVox directly
 BOUYOMI_VOLUME = 50
 LIST_VOICEVOX_SPEAKERS_ON_START = False #Print a list of all availiable VoiceVox voices and their IDs
-DEFAULT_VOICE = 35 #Default voice ID used for Bouyoumi or VoiceVox
+DEFAULT_VOICE = 34 #Default voice ID used for Bouyoumi or VoiceVox
 DEBUG_INFO = False #Will desplay raw message data
 PRINT_MESSAGES = True #Will print chat messages
+PRINT_PLAYED_MESSAGE_SOUND = True #Will print paths of played sounds
 REMOVE_EMOTE = True
+REMOVE_EMOJI = True
+DEFAULT_MESSAGE_SOUND = True #Play /sounds/default.wav if message is blank after formating (ex: emote only)
+SINGLE_EMOTE_SOUND = True #Play /sounds/<EMOTENAME>.wav if message consists of only one emote/string of one emote
+MESSAGE_SOUND_VOLUME = 0.5 # from 0 to 1
 IGNORE_ACTION_MESSAGES = False #Ignore messages that start with \001ACTION. Usually bot messages...
 #^When false, just removes SOH and ACTION. 
 
@@ -35,7 +36,14 @@ MESSAGE_SEPARATOR = "."#placed at the end of messages to be read...
 REPLACE_SPACES = ' ' #Replaces spaces with a char or string, useful for japanse voice syths
 #^ recomened options (do not replace space)space:' ', Kuten:'。', (removeAllSpaces)nothing:''
 
-
+if USE_BOUYOMICHAN:
+    import bouyomichan
+else:
+    from voicevox import Client
+    import pyaudio
+    
+if DEFAULT_MESSAGE_SOUND or SINGLE_EMOTE_SOUND:
+    import pyaudio
 
 async def chatLoop():
     pass
@@ -48,6 +56,37 @@ async def speakerList():
             for style in speaker.styles:
                 print("   " + style.name + " : " + str(style.id))
 
+def change_volume(data, volume):
+    if volume > 1 or volume < 0:
+        raise Exception("MESSAGE_SOUND_VOLUME must be between 1 and 0")
+    # Unpack the binary data into numerical values
+    unpacked_data = struct.unpack(f"{len(data)//2}h", data)
+    # Scale the numerical values by the volume factor
+    scaled_data = [int(x * volume) for x in unpacked_data]
+    # Pack the scaled numerical values back into binary data
+    return struct.pack(f"{len(scaled_data)}h", *scaled_data)
+
+def playSound(soundPath, volume):
+    # Open wave (1)
+    with wave.open(soundPath, "rb") as rf:
+        p = pyaudio.PyAudio()
+
+        # Open stream (2)
+        stream = p.open(format=p.get_format_from_width(rf.getsampwidth()),
+                        channels=rf.getnchannels(),
+                        rate=rf.getframerate(),
+                        output=True)
+
+        # Play samples from the wave file (3)
+        while len(data := rf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
+            stream.write(change_volume(data,volume))
+
+        # Close stream (4)
+        stream.close()
+
+        # Release PortAudio system resources (5)
+        p.terminate()
+
 async def readMessageVoiceVox(text, messageID, speakerID):
     fileName = messageID + ".wav"
     async with Client() as client:
@@ -56,24 +95,7 @@ async def readMessageVoiceVox(text, messageID, speakerID):
         )
         with open(fileName, "wb") as wf:
             wf.write(await audio_query.synthesis(speaker=speakerID))
-        with wave.open(fileName, "rb") as rf:
-            p = pyaudio.PyAudio()
-
-            # Open stream (2)
-            stream = p.open(format=p.get_format_from_width(rf.getsampwidth()),
-                            channels=rf.getnchannels(),
-                            rate=rf.getframerate(),
-                            output=True)
-
-            # Play samples from the wave file (3)
-            while len(data := rf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
-                stream.write(data)
-
-            # Close stream (4)
-            stream.close()
-
-            # Release PortAudio system resources (5)
-            p.terminate()
+        playSound(fileName,1)
 
         os.remove(fileName)
 
@@ -94,16 +116,29 @@ def parse(msg):
         emoteLists = re.findall(r'([0-9]+-[0-9]+)', emoteTag)
         emoteLists = [pair.split('-') for pair in emoteLists]
         emoteLists.sort(key=lambda x: int(x[0]))
+        emoteSet = set()
         if REMOVE_EMOTE:
             formatedMsg = ''
             prevInx = 0;
             for pair in emoteLists:
+                emoteSet.add(rawMsg[int(pair[0]):int(pair[1])+1])
                 formatedMsg = formatedMsg + rawMsg[prevInx:int(pair[0])]
                 prevInx = int(pair[1])+1
             formatedMsg = formatedMsg + rawMsg[prevInx:]
         else:
             formatedMsg = rawMsg
 
+        if REMOVE_EMOJI:
+            emoji_pattern = re.compile("["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                u"\U0001F900-\U0001F9FF"  # Supplemental Symbols
+                                    "]+", flags=re.UNICODE)
+            
+            formatedMsg = emoji_pattern.sub(r'', formatedMsg) # no emoji
+            
         if re.match('^\001ACTION', formatedMsg):
             if IGNORE_ACTION_MESSAGES:
                 return None
@@ -120,11 +155,12 @@ def parse(msg):
                 'message' : formatedMsg,
                 'ID': ID,
                 'tags' : splitMsg[0],
-                'emotePosList' : emoteLists
+                'emotePosList' : emoteLists,
+                'emoteSet' : emoteSet
             }
     else:
         return None
-    
+
 
 if __name__ == "__main__":
     if LIST_VOICEVOX_SPEAKERS_ON_START and not USE_BOUYOMICHAN:
@@ -215,9 +251,28 @@ if __name__ == "__main__":
                 if PRINT_MESSAGES: 
                     print(name + ": " + msg['message'])
                     if DEBUG_INFO:
+                        print("Emotes: " + str(msg['emoteSet']))
                         print("Formated Length: " + str(len(msg['message'])))
                 voice = voiceDict.get(msg['username'], DEFAULT_VOICE)
                 speech.append([msg['message'] + MESSAGE_SEPARATOR, msg['ID'],voice])
+                
+                if (DEFAULT_MESSAGE_SOUND or SINGLE_EMOTE_SOUND) and len(msg['message']) == 0:
+                    if DEFAULT_MESSAGE_SOUND:
+                        soundPath = 'sounds/default.wav'
+                    else:
+                        soundPath = ''
+
+                    if SINGLE_EMOTE_SOUND and len(msg['emoteSet']) == 1:
+                        emoteSoundPath = 'sounds/' + next(iter(msg['emoteSet'])) + '.wav'
+                        if DEBUG_INFO:
+                            print("Tried to find '" + emoteSoundPath + "'.")
+                        if os.path.exists(emoteSoundPath):
+                            soundPath = emoteSoundPath
+                    
+                    if soundPath != '':
+                        if DEBUG_INFO or PRINT_PLAYED_MESSAGE_SOUND:
+                            print("Playing sound '" + soundPath + "' at " + str(MESSAGE_SOUND_VOLUME * 100) + "% Volume")
+                        playSound(soundPath, MESSAGE_SOUND_VOLUME)
                             
             for msg in messages:
                 name = msg['displayname']
@@ -234,6 +289,7 @@ if __name__ == "__main__":
                     else:
                         speech[-1][0] = speech[-1][0] +  msg['message'] + MESSAGE_SEPARATOR
 
+                
             for linePair in speech:
                 if len(linePair[0]) > 0:
                     if DEBUG_INFO:
